ed8d7248fad9194d8a1f48802f4ea8852401bc6d
aubrxyy <aubreymantiri@gmail.com>
2024-10-24 17:49:09 +0700

    header update

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,48 @@
 'use client'
 
-import "./_components/Header"
-import Header from "./_components/Header"
 import { useRouter } from 'next/navigation';
+import "./_components/Header";
+import Header from "./_components/Header";
 import { setCookie } from './utils/cookies';
+import { Inter } from 'next/font/google';
+import Image from 'next/image';
+
+const interM = Inter({
+  subsets: ['latin'],
+  weight: '500',
+})
+
+const interR = Inter({
+  subsets: ['latin'],
+  weight: '400',
+})
 
 export default function Home() {
   const router = useRouter();
 
   const handleLogout = () => {
-    // Clear cookies
     setCookie('currentUser', '', -1);
     setCookie('userRole', '', -1);
-    // Clear local storage
-    localStorage.removeItem('token');
-    // Redirect to login page
+    localStorage.removeItem('token'); 
     router.push('/login');
   };
 
   return (
     <>
       <Header />
-      <div className="gradient-bg w-full h-[67.5vh]"></div>
+      <div className={`gradient-bg w-full h-[67.5vh] ${interR.className}`}>
+        <div className="container mx-auto flex flex-col lg:flex-row h-full justify-center items-center px-4 lg:px-0">
+          <div className="flex justify-center flex-col text-white text-center lg:text-left mb-8 lg:mb-0">
+            <div className="opacity-50 text-2xl lg:mb-4 ">Betta</div>
+            <div className="text-5xl lg:text-8xl lg:mb-4 mb-2">Crowntail</div>
+            <div className="w-full lg:w-[60ch] mb-4">Cupang crown tail menjadi mendunia karena variasi keindahannya. Disebut crown tail atau ekor mahkota, karena bila dibalik menghadap ke atas serit-serit pada ekornya terlihat seperti mahkota raja.</div>
+            <button className="border-[1px] w-fit px-8 py-3 rounded-md border-white mx-auto lg:mx-0">Shop now</button>
+          </div>
+          <div className="flex justify-center">
+            <Image src="/herofish1.png" alt="Ikan" width={600} height={600} className="w-full max-w-xs lg:max-w-none lg:w-auto"/>
+          </div>
+        </div>
+      </div>
       <div className="pt-4">
         <button onClick={handleLogout} className="bg-red-500 text-white p-2 rounded flex mx-auto">Logout</button>
       </div>
